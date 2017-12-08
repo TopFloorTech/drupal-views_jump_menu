695f832783ae1a2a2f61d4316ac48c100fde1a23
sludwig <sludwig@816958.no-reply.drupal.org>
2017-12-08 17:22:18 -0600

    Issue #2916097 by sludwig: JS-usage of Drupal / Drupal.behaviors should be declared

--- js/views-jump-menu.js
+++ js/views-jump-menu.js
@@ -1,11 +1,17 @@
-(function ($) {
+/**
+ * @file
+ * Attaches the behaviors for the Views-Jump-Menu module.
+ */
+
+(function ($, Drupal) {
   'use strict';
 
   Drupal.behaviors.viewsJumpMenu = {
     attach: function (context, settings) {
-      $('.js-viewsJumpMenu').on('change', function () {
+      $('.js-viewsJumpMenu', context).on('change', function () {
         window.location = $(this).find(':selected').data('url');
       });
     }
   };
-}(jQuery));
+
+})(jQuery, Drupal);
